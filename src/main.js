d48cd2a8f33cf435d5526806b9dfb29058ee486b
Mr-zjp <2656251622@qq.com>
2019-12-13 16:53:25 +0800

    vue vw

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-
-Vue.config.productionTip = false
-
+import config from './utils/config';
+Vue.config.productionTip = false;
+Vue.prototype.$config=config;
 new Vue({
   render: h => h(App),
 }).$mount('#app')
